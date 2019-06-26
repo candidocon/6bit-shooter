7e393bc1f0c57da7d865651ee60d7512ae7c067c
candidocon <abrahan64@gmail.com>
2019-06-26 14:21:30 -0400

    second commit

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ window.onload = function() {
     startGame($(this).data("players"));
     $("#title-screen").hide();
   });
-
+  $("#how-to-play-btn").click(function() {
+    $("#title-screen").hide();
+    $("#how-to-play-div").show();
+  });
   document.onkeydown = function(e) {
     if (e.keyCode === 65 /*a*/) {
       player1.switchDiretion();
@@ -37,13 +40,22 @@ class PLayer1 {
     this.direction = "down";
     this.canShoot = true;
     this.bullets = 6;
+    this.lives = 3;
+    this.wins = 0;
   }
   drawItself() {
     ctx.fillStyle = "#FF0000";
     // ctx.fillRect(this.x, this.y, this.width, this.height);
     var myImg = new Image();
-    myImg.src = "images/player1.png";
-    ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+    if (!this.canShoot) {
+      myImg.src = "images/player1-reloading.png";
+      ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+      ctx.font = "20pt Calibri";
+      ctx.fillText("RELOADING", this.x, this.y + this.height + 20);
+    } else {
+      myImg.src = "images/player1.png";
+      ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+    }
   }
   switchDiretion() {
     if (this.direction === "down") {
@@ -82,29 +94,50 @@ class PLayer1 {
         }
       } else if (this.direction === "up") {
         this.y -= 5;
-        if (this.y <= 0) {
+        if (this.y <= 75) {
           this.direction = "down";
         }
       }
     }, 20);
   }
+  updateBoard() {
+    var myImg = new Image();
+    myImg.src = "images/heart-pixel-art-32x32.png";
+    for (let i = 0; i < this.lives; i++) {
+      ctx.drawImage(myImg, i * 35 + 35, 20, 32, 32);
+    }
+    // ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "#FF0000";
+    ctx.font = "60px Arial";
+    ctx.fillText(this.wins, 470, 60);
+  }
 }
 class PLayer2 {
   constructor() {
     this.x = 850;
-    this.y = 0;
+    this.y = 75;
     this.width = 100;
     this.height = 100;
     this.direction = "up";
     this.human = true;
+    this.difficulty = 1;
     this.canShoot = true;
     this.bullets = 6;
+    this.lives = 3;
+    this.wins = 0;
   }
   drawItself() {
     ctx.fillStyle = "#19bbc4";
     var myImg = new Image();
-    myImg.src = "images/player2.png";
-    ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+    if (!this.canShoot) {
+      myImg.src = "images/player2-reloading.png";
+      ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+      ctx.font = "20pt Calibri";
+      ctx.fillText("RELOADING", this.x, this.y + this.height + 20);
+    } else {
+      myImg.src = "images/player2.png";
+      ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+    }
   }
   switchDiretion() {
     if (this.direction === "down") {
@@ -144,17 +177,28 @@ class PLayer2 {
         }
       } else if (this.direction === "up") {
         this.y -= 5;
-        if (this.y <= 0) {
+        if (this.y <= 75) {
           this.direction = "down";
         }
       }
     }, 20);
   }
+  updateBoard() {
+    var myImg = new Image();
+    myImg.src = "images/heart-pixel-art-32x32.png";
+    for (let i = 0; i < this.lives; i++) {
+      ctx.drawImage(myImg, 933 - i * 35, 20, 32, 32);
+    }
+    // ctx.drawImage(myImg, this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "#19bbc4";
+    ctx.font = "60px Arial";
+    ctx.fillText(this.wins, 530, 60);
+  }
   activatePC() {
     setInterval(() => {
-      let randomDodge = Math.floor(Math.random() * 40);
+      let randomDodge = Math.floor(Math.random() * (40 - this.difficulty * 10));
 
-      let randomShoot = Math.floor(Math.random() * 50);
+      let randomShoot = Math.floor(Math.random() * (40 - this.difficulty * 10));
 
       if (randomDodge === 2) {
         this.switchDiretion();
@@ -193,6 +237,8 @@ class Projectile {
 function drawEverything() {
   player1.drawItself();
   player2.drawItself();
+  player1.updateBoard();
+  player2.updateBoard();
 
   allTheProjectiles.forEach(eachProjectile => {
     eachProjectile.drawItself();
@@ -206,10 +252,20 @@ function detectCollisions() {
       player1.y < Projectile.y + Projectile.height &&
       player1.y + player1.height > Projectile.y
     ) {
-      // setTimeout(() => {
-      alert("Player 2 wins");
-      location.reload();
-      // }, 50);
+      Projectile.y += 1000; //remove bullet
+      //player 1 gets hit
+      player1.lives--;
+      if (player1.lives === 0) {
+        player2.wins++;
+        setTimeout(() => {
+          alert("Player 2 wins");
+          $("#title-screen").show();
+          $("#retry").show();
+          $("#start").hide();
+          // location.reload();
+          restartGame();
+        }, 50);
+      }
     }
     if (
       player2.x < Projectile.x + Projectile.width &&
@@ -217,10 +273,17 @@ function detectCollisions() {
       player2.y < Projectile.y + Projectile.height &&
       player2.y + player2.height > Projectile.y
     ) {
-      // setTimeout(() => {
-      alert("Player 1 Wins");
-      location.reload();
-      // }, 50);
+      Projectile.y += 1000; //remove bullet
+      //player 2 gets hit
+      player2.lives--;
+      if (player2.lives === 0) {
+        player1.wins++;
+        setTimeout(() => {
+          alert("Player 1 wins");
+          // location.reload();
+          restartGame();
+        }, 50);
+      }
     }
   });
 }
@@ -231,11 +294,13 @@ function animate() {
     detectCollisions();
   }, 16);
 }
+
 function startGame(players) {
   console.log(players);
   player1 = new PLayer1();
   player1.drawItself();
   player1.move();
+  player1.updateBoard();
 
   player2 = new PLayer2();
   player2.drawItself();
@@ -246,4 +311,20 @@ function startGame(players) {
     player2.human = false;
     player2.activatePC();
   }
+}
+function restartGame() {
+  allTheProjectiles = [];
+  player1.lives = 3;
+  if (player2.human) {
+    player2.lives = 3;
+  } else {
+    if (player1.wins < 5) {
+      player2.lives = player1.wins + 3;
+    }
+    if (player1.wins < 3) {
+      player2.difficulty = 1 + player1.wins;
+    }
+  }
+  player1.bullets = 6;
+  player2.bullets = 6;
 }
